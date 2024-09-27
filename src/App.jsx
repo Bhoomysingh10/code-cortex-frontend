2202eebc71033e66b6f33378558277bdfe349b82
Bhoomysingh10 <bhoomysingh123@gmail.com>
2024-09-27 16:13:07 +0530

    made the frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import FirstPage from './Components/FirstPage'
+import DashBoard from './Components/DashBoard'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <FirstPage/>
+      <DashBoard/>
     </div>
   )
 }
---
+++ src/Components/DashBoard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function DashBoard() {
+  return (
+    <div className="relative h-screen flex items-center justify-center bg-[#282231]">
+      <img 
+        src="image.png" 
+        alt="Dashboard" 
+        className="absolute w-auto max-w-full h-auto max-h-full px-4 m-4"
+        style={{
+          top: '25%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+    </div>
+  );
+}
+
+export default DashBoard;
