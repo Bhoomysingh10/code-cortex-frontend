b31d33d5f5a0aba212fdba44eda5f1d1c35a7c29
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-09-27 17:51:50 +0530

    configure: Configured the absolute placement.

--- src/sections/homePage.jsx
+++ src/sections/homePage.jsx
@@ -7,12 +7,12 @@ const HomePage = () => {
         <Navbar/>
         <div className='h-[100vh] w-[100vw] bg-gradient-to-b from-[#1A161E] to-[#282231]'>
             <div className=''>
-                <div className='bg-black w-[40rem] h-[15rem] rounded-xl absolute top-[10rem] left-[5rem] flex items-center p-5 font-extrabold text-white text-[42px] leading-tight'>
+                <div className='bg-black w-[40rem] h-[15rem] rounded-xl absolute top-[20%] left-[5rem] flex items-center p-5 font-extrabold text-white text-[42px] leading-tight'>
                 Revolutionizing Wellness: Detect Mental Fatigue with AI
                 </div>  
             </div>
 
-            <div className='bg-[#5e558e76] h-[15rem] w-[38rem] absolute bottom-[10rem] rounded-xl left-[5rem] p-5 text-white text-[42px] flex flex-col gap-3'>
+            <div className='bg-[#5e558e76] h-[15rem] w-[38rem] absolute bottom-[16%] rounded-xl left-[5rem] p-5 text-white text-[42px] flex flex-col gap-3'>
                 <p>15/17
                 2025</p>
                 <p className='font-bold'>OCTOBER</p>
